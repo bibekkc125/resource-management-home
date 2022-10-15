70ec601d766425f3edd356a95ba4b1c72f1e64c8
Bibek <fm-pc-lt-159@FM-PC-LT-159s-MacBook-Pro.local>
2022-10-15 21:20:54 +0545

    add:home view for uploaded files

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    borderWidth: {
+      1: "1.5px",
+    },
+    colors: {
+      primary: "#008778",
+      primaryLight: "#E6F4F2",
+      danger: "#E44040",
+      dangerLight: "#FDECEC",
+      heading: "#161F1E",
+      bodyCopy: "#303A39",
+      subtleText: "#5D6867",
+      subtleText2: "#86908F",
+      formFields: "#B6BFBE",
+      hairlines: "#D5DEDD",
+      hairlines2: "#E6EBEA",
+      bgColor: "#F7F7F7",
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    extend: { spacing: { 9: "2.25rem" } },
+  },
+  variants: {},
+  plugins: [],
+};
